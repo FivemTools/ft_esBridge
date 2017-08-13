766072b6ac4b5044010dad0b5cd463db7550575a
samuelds <samuelds.pro@gmail.com>
2017-08-13 13:30:28 +0200

    Add ft_players

--- __resource.lua
+++ __resource.lua
@@ -2,11 +2,15 @@
 -- @License: GNU General Public License v3.0
 -- @Source: https://github.com/FivemTools/ft_esBridge
 
+dependencies {
+  "ft_players",
+}
 
 client_scripts {
 	"cl_client.lua",
 }
 
 server_scripts {
-	"sv_player.lua"	
+	"@ft_players/server.wrappers.lua",
+	"sv_player.lua",
 }
--- sv_player.lua
+++ sv_player.lua
@@ -1,53 +1,42 @@
+--
 -- @Project: FiveM Tools
 -- @License: GNU General Public License v3.0
 -- @Source: https://github.com/FivemTools/ft_esBridge
-
--- Fake data for all es version
-function Player(data)
-
-	local self = {}
-
-	self.get = function (key)
-		return self[key]
-	end
-
-	for key, value in ipairs(data) do
-		self[key] = value
-	end
-
-	return self
-
-end
+--
 
 -- Return all Users
 AddEventHandler('es:getPlayers', function (callback)
-
+	local players = GetPlayers()
+	callback(players)
 end)
-
 
 -- Get all player in database
 AddEventHandler("es:getAllPlayers", function (callback)
-
-
+	callback(nil)
 end)
 
 -- Set value in database by player
-AddEventHandler("es:setPlayerData", function(player, key, value, callback)
-
+AddEventHandler("es:setPlayerData", function(id, key, value, callback)
+	local player = GetPlayerFormId(id)
+	player:Set(key, value)
+	callback("Player data edited.", true)
 end)
 
 -- Set value in database by identifier
-AddEventHandler("es:setPlayerDataId", function (identifier, key, value, callback)
-
+AddEventHandler("es:setPlayerDataId", function (steamId, key, value, callback)
+	local player = GetPlayerFormSteamId(identifier)
+	player:Set(key, value)
+	callback("Player data edited.", true)
 end)
 
-
 -- Get values in players by player
-AddEventHandler("es:getPlayerFromId", function (player, callback)
-
+AddEventHandler("es:getPlayerFromId", function (id, callback)
+	local player = GetPlayerFormId(id)
+	callback(player)
 end)
 
 -- Get values in database by identifier
-AddEventHandler("es:getPlayerFromIdentifier", function (identifier, callback)
-
+AddEventHandler("es:getPlayerFromIdentifier", function (steamId, callback)
+		local player = GetPlayerFormSteamId(identifier)
+		callback(player)
 end)
